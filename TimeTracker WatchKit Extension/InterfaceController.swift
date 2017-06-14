6303fad77c6e16961dd62ae7986aa53f2c4e3163
Vandan Patel <vandan.ets@gmail.com>
2017-06-14 11:31:18 -0400

    Debugging

--- TimeTracker WatchKit Extension/InterfaceController.swift
+++ TimeTracker WatchKit Extension/InterfaceController.swift
@@ -46,57 +46,75 @@ class InterfaceController: WKInterfaceController {
             clockOut()
         } else {
             clockIn()
-            
         }
     }
     
     func clockIn() {
-        updateUI(clockedIn: clockedIn)
-        clockedIn = true
-        UserDefaults.standard.set(Date(), forKey: "clockedIn")
-        UserDefaults.standard.synchronize()
-        
-        Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { (timer) in
-            guard let clockedInDate = UserDefaults.standard.value(forKey: "clockedIn") as? Date else { return }
-            let timeInterval = Int(Date().timeIntervalSince(clockedInDate))
-            
-            let hours = timeInterval / 3600
-            let minutes = (timeInterval % 3600) / 60
-            let seconds = timeInterval % 60
-            
-            self.middleLabel.setText("\(hours)h \(minutes)m \(seconds)s")
-        }
+        updateUI(clockedIn: true)
+        //        UserDefaults.standard.set(Date(), forKey: "clockedIn")
+        //        UserDefaults.standard.synchronize()
+        //
+        //        Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { (timer) in
+        //            guard let clockedInDate = UserDefaults.standard.value(forKey: "clockedIn") as? Date else { return }
+        //            let timeInterval = Int(Date().timeIntervalSince(clockedInDate))
+        //
+        //            let hours = timeInterval / 3600
+        //            let minutes = (timeInterval % 3600) / 60
+        //            let seconds = timeInterval % 60
+        //
+        //            self.middleLabel.setText("\(hours)h \(minutes)m \(seconds)s")
+        //
+        //            let totalTimeInterval = timeInterval + self.totalClockedTime()
+        //
+        //            let totalHours = totalTimeInterval / 3600
+        //            let totalMinutes = (totalTimeInterval % 3600) / 60
+        //            let totalSeconds = totalTimeInterval % 60
+        //
+        //            self.topLabel.setText("\(totalHours)h \(totalMinutes)m \(totalSeconds)s")
+        //        }
     }
     
     func clockOut() {
         
-        updateUI(clockedIn: clockedIn)
-        clockedIn = false
-        guard let clockedInDate = UserDefaults.standard.value(forKey: "clockedIn") as? Date else { return }
-        
-        guard var clockIns = UserDefaults.standard.array(forKey: "clockIns") as? [Date] else
-        {
-            UserDefaults.standard.set([clockedInDate], forKey: "clockIns")
-            return
-        }
-        
-        clockIns.insert(clockedInDate, at: 0)
-        UserDefaults.standard.set(clockIns, forKey: "clockIns")
-        
-        guard var clockOuts = UserDefaults.standard.array(forKey: "clockOuts") as? [Date] else {
-            UserDefaults.standard.set([Date()], forKey: "clockOuts")
-            return
-        }
-        
-        clockOuts.insert(Date(), at: 0)
-        UserDefaults.standard.set(clockOuts, forKey: "clockOuts")
-        
-        UserDefaults.standard.set(nil, forKey: "clockedIn")
-        
-        UserDefaults.standard.synchronize()
+        updateUI(clockedIn: false)
+        clockedIn = true
+        //        guard let clockedInDate = UserDefaults.standard.value(forKey: "clockedIn") as? Date else { return }
+        //
+        //        if var clockIns = UserDefaults.standard.array(forKey: "clockIns") as? [Date] {
+        //            clockIns.insert(clockedInDate, at: 0)
+        //            UserDefaults.standard.set(clockIns, forKey: "clockIns")
+        //        } else {
+        //            UserDefaults.standard.set([clockedInDate], forKey: "clockIns")
+        //        }
+        //
+        //        if var clockOuts = UserDefaults.standard.array(forKey: "clockOuts") as? [Date] {
+        //            clockOuts.insert(Date(), at: 0)
+        //            UserDefaults.standard.set(clockOuts, forKey: "clockOuts")
+        //        } else {
+        //            UserDefaults.standard.set([Date()], forKey: "clockOuts")
+        //        }
+        //
+        //        UserDefaults.standard.set(nil, forKey: "clockedIn")
+        //        UserDefaults.standard.synchronize()
     }
     
-    
+    func totalClockedTime() -> Int {
+        
+        guard let clockedIns = (UserDefaults.standard.array(forKey: "clockIns") as? [Date])  else { return 0 }
+        guard let clockOuts = (UserDefaults.standard.array(forKey: "clockOuts") as? [Date])  else { return 0 }
+        
+        print("ClockedIns : \(clockedIns.count)")
+        print("ClockedOuts : \(clockOuts.count)")
+        
+        var seconds = 0
+        
+        for index in 0..<clockedIns.count {
+            let currentInterval = Int(clockOuts[index].timeIntervalSince(clockedIns[index]))
+            seconds += currentInterval
+        }
+        
+        return seconds
+    }
     
     
     
